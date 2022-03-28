c58c57d9a62ccc7e842a705578b3cca287ccf363
jek181 <70042601+jek181@users.noreply.github.com>
2022-03-28 13:15:26 -0400

    Update serialCommnication.ino

--- Hardware/serialCommnication.ino
+++ Hardware/serialCommnication.ino
@@ -22,14 +22,59 @@ void setup() {
 
 
 void loop() {
-  if(Serial.available() > 0)
+ if(Serial.available() > 0)
   {
     int number = Serial.read() - '0';
     switch (number) {
+      case 0:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Zero.wav");
+        delay(2000);
+        tmrpcm.disable();
+      case 1:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("One.wav");
+        delay(2000);
+        tmrpcm.disable();
+      case 2:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Two.wav");
+        delay(2000);
+        tmrpcm.disable();
+      case 3:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Three.wav");
+        delay(1000);
+        tmrpcm.disable();
+      case 4:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Four.wav");
+        delay(2000);
+        tmrpcm.disable();
+      case 5:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Five.wav");
+        delay(2000);
+        tmrpcm.disable();
+      case 6:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Six.wav");
+        delay(2000);
+        tmrpcm.disable();
       case 7:
         tmrpcm.setVolume(6);
-        tmrpcm.play("test.wav");
-        delay(1000);
+        tmrpcm.play("Seven.wav");
+        delay(2000);
+        tmrpcm.disable();
+      case 8:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Eight.wav");
+        delay(2000);
+        tmrpcm.disable();
+      case 9:
+        tmrpcm.setVolume(6);
+        tmrpcm.play("Nine.wav");
+        delay(2000);
         tmrpcm.disable();
       default:
         tmrpcm.disable();
